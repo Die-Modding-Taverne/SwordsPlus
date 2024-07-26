25b9dd61d153ded7cb47a16e28b5125745359b8f
M0re <semih.schneeweiss@aol.de>
2024-07-26 23:39:36 +0200

    first commit

---
+++ src/client/java/de/dmt/swordsplus/client/SwordsplusClient.java
@@ -0,0 +1,10 @@
+package de.dmt.swordsplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwordsplusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/dmt/swordsplus/Swordsplus.java
@@ -0,0 +1,10 @@
+package de.dmt.swordsplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Swordsplus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
